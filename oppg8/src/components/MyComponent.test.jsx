e15f95ba53258be16341d2c3e8f42ddd189a2fd5
FuzzyCookie97 <69955315+FuzzyCookie97@users.noreply.github.com>
2021-10-18 18:03:10 +0200

    Changed the tests

--- oppg8/src/components/MyComponent.test.jsx
+++ oppg8/src/components/MyComponent.test.jsx
@@ -3,7 +3,27 @@ import { render } from '@testing-library/react'
 import { MyComponent } from "./MyComponent"
 
 
-it('<p> should change between true and false strings during onClick', () => {
+  it('<p> Should change to false string during onClick', () => {
+    render(<MyComponent />)
+  
+    expect(document.querySelector('p')).toHaveTextContent('')
+
+    fireEvent.click(screen.getByText('false'))
+
+    expect(document.querySelector('p')).toHaveTextContent('false')
+  })
+
+  it('<p> Should change to true string during onClick', () => {
+    render(<MyComponent />)
+  
+    expect(document.querySelector('p')).toHaveTextContent('')
+
+    fireEvent.click(screen.getByText('true'))
+
+    expect(document.querySelector('p')).toHaveTextContent('true')
+  })
+
+  it('<p> should change between true and false strings during onClick', () => {
     render(<MyComponent />)
   
     expect(document.querySelector('p')).toHaveTextContent('')
